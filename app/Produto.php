0b4b001f18566a124b28d0e928088e7a52e7a88f
Gabriel Fidelis Pereira <83375130+FidelisGf@users.noreply.github.com>
2024-03-05 12:37:00 -0300

    add Imagem in product

--- app/Produto.php
+++ app/Produto.php
@@ -8,16 +8,26 @@ class Produto extends Model
 {
     protected $table = 'Produto';
     protected $primaryKey = 'idProduto';
-    protected $fillable = ['Nome_produto', 'Preco_compra', 'Preco_venda', 'Descricao'];
+    protected $fillable = ['Nome_produto', 'Preco_compra', 'Preco_venda', 'Descricao', 'Imagem'];
     public $timestamps = true;
 
     public function entradaProduto()
     {
         return $this->hasOne('App\EntradaProduto', 'idProduto', 'idProduto');
     }
-    
+
     public function itensMesa()
     {
         return $this->belongsToMany('App\ItensMesa', 'idProduto', 'idProduto');
+    }
+
+    public function setImagemAttribute($value)
+    {
+        $this->attributes['Imagem'] = json_encode($value);
+    }
+
+    public function getImagemAttribute($value)
+    {
+        return json_decode($value, true);
     }
 }
